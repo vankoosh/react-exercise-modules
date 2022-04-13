88a464e5e45f28e31d0e9ca5a599d37bb6d52b2d
vankoosh <vankoosh@users.noreply.github.com>
2022-04-13 16:03:09 -0100

    my first component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import P from './components/p.js';
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <P/>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/components/p.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class P extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <p>Hello World</p>
+      </React.Fragment>
+    )
+  }
+}
+
+export default P
