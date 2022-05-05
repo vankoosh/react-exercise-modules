0ccfc7f777f2e65a2ea0a429a5ad0b9294c16a29
vankoosh <vankoosh@users.noreply.github.com>
2022-05-05 15:35:55 -0100

    state set title

---
+++ src/components/test.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+export default function RenameTitle() {
+  const [title, setTitle] = useState({ title: "" });
+
+  setTitle()
+
+  return (<div>
+    <p>Set your title here</p>
+    <input
+      onChange={({target}) => {
+        setTitle({
+          title: target.value
+        })
+      }}
+      value={title}
+      type="text"
+    ></input>
+  </div>);
+}
