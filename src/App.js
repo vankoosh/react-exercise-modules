9bdead779390827c6317d779a46c921b7cccfb69
vankoosh <vankoosh@users.noreply.github.com>
2022-05-10 19:12:29 -0100

    parent child sibling excercise

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import Parent from "./components/NamePicker";
+import Parent from "./components/ParentChildSibling";
 
 export default function App() {
   return (
