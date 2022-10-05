1efb0d6e8560ba1fe0f8d89eb226758141f1c3fa
vankoosh <vankoosh@gmail.com>
2022-10-05 23:39:51 +0000

    new comment

--- src/components/PassingThoughts/ThoughtApp.jsx
+++ src/components/PassingThoughts/ThoughtApp.jsx
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import Thought from './Thought';
 import AddThoughtForm from './AddThoughtForm';
+// hello
 
 // UTILITIES FUNCTIONS
 function getNewExpirationTime() {
