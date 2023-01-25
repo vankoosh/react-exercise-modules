29d9c8c4bce0d5cac8bb52280b4e8b129b441530
vankoosh <vankoosh@gmail.com>
2023-01-25 12:16:40 +0000

    pic loop repaired

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import X from "./components/PickPizzaTopping";
+import X from "./components/PicCarousel";
 
 export default function App() {
   return (
--- src/components/PicCarousel.jsx
+++ src/components/PicCarousel.jsx
@@ -1,60 +1,36 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
 // THIS IS ALSO AN EXAMPLE OF SEPARATING PRESENTATIONAL COMPONENTS FROM CONTAINER COMPONENTS
 // THE FIRST COMPONENT IS PRESENTATIONAL, IT ONLY RENDERS THE JSX
-class GuineaPigs extends React.Component {
-  render() {
-    let src = this.props.src;
-    return (
-      <div>
-        <h1>Cute Guinea Pigs</h1>
-        <img src={src} />
-      </div>
-    );
-  }
-};
-
+function Pics({ src }) {
+  return (
+    <div>
+      <h1>Cute Guinea Pigs</h1>
+      <img src={src} alt="pig" />
+    </div>
+  );
+}
 
 // THE NEXT COMPONENT IS A CONTAINER COMPONENT AND CONTAINS THE LOGIC BEFORE FINAL RENDER
 // AND SHOULD IMPORT THE PRESENTATIONAL COMPONENT FIRST TO USE IT
 
-const GUINEAPATHS = [
+const PICS = [
   "https://content.codecademy.com/courses/React/react_photo-guineapig-1.jpg",
   "https://content.codecademy.com/courses/React/react_photo-guineapig-2.jpg",
   "https://content.codecademy.com/courses/React/react_photo-guineapig-3.jpg",
   "https://content.codecademy.com/courses/React/react_photo-guineapig-4.jpg",
 ];
 
-export default class GuineaPigsContainer extends React.Component {
-  constructor(props) {
-    super(props);
+export default function PicsContainer() {
+  const [currentPic, setcurrentPic] = useState(0);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      currentPic < PICS.length -1
+        ? setcurrentPic((prev) => prev + 1)
+        : setcurrentPic(0);
+    }, 2000);
+    return () => clearInterval(interval);
+  }, [currentPic]);
 
-    this.state = { currentGP: 0 };
-
-    this.interval = null;
-
-    this.nextGP = this.nextGP.bind(this);
-  }
-
-  nextGP() {
-    let current = this.state.currentGP;
-    let next = ++current % GUINEAPATHS.length;
-    this.setState({ currentGP: next });
-  }
-
-  componentDidMount() {
-    this.interval = setInterval(this.nextGP, 5000);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
-
-  render() {
-    const src = GUINEAPATHS[this.state.currentGP];
-    return <GuineaPigs src={src} />;
-  }
+  return <Pics src={PICS[currentPic]} />;
 }
-
-
-
